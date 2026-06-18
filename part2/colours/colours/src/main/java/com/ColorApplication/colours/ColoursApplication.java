bdb26bb6d1dd5d3d30ec31315993f3027b5e582a
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-06-18 14:22:15 -0500

    Added the starter for this application

--- part2/colours/colours/src/main/java/com/ColorApplication/colours/ColoursApplication.java
+++ part2/colours/colours/src/main/java/com/ColorApplication/colours/ColoursApplication.java
@@ -1,13 +1,27 @@
 package com.ColorApplication.colours;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*	***Notice***
+	This is a SpringBoot Command Line Runner Application.
+	Main purpose: To understand Beans and how they work.
+	Note: the run method main objective is to run the application and display
+	the correct color based on language: French, English, Spanish.
+*/
 @SpringBootApplication
-public class ColoursApplication {
+@Log
+public class ColoursApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ColoursApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ColoursApplication.class, args);
+  }
 
+  @Override
+  public void run(final String... args) {
+    final ColourPrinter ColourPrinter = new ColourPrinterImpl();
+    log.info(colourPrinter.print());
+  }
 }
