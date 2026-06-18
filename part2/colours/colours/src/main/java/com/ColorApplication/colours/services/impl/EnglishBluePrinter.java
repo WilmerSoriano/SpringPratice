e9e0859ddc3ae58de563e19ad71babeb9dbd3edf
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-06-18 14:51:33 -0500

    Added the mechanics for English

---
+++ part2/colours/colours/src/main/java/com/ColorApplication/colours/services/impl/EnglishBluePrinter.java
@@ -0,0 +1,10 @@
+package com.ColorApplication.colours.services.impl;
+
+import com.ColorApplication.colours.services.BluePrinter;
+
+public class EnglishBluePrinter implements BluePrinter {
+  @Override
+  public String print() {
+    return "Blue";
+  }
+}
