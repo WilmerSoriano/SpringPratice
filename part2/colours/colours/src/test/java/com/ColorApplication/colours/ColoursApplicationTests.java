4109a2a4a185da462133e46fbb18d527b88212d6
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-06-18 13:52:42 -0500

    new project name: colours, colors, colores with Beans

---
+++ part2/colours/colours/src/test/java/com/ColorApplication/colours/ColoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ColorApplication.colours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
