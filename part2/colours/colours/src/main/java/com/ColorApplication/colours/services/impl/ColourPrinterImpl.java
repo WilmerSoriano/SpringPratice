c0977f7795d3c2cc64a9f9732c8e80b0dc03f177
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-06-18 14:44:53 -0500

    explaining why we shouldn't make an instance of color in the constructor. It's to tight to coupaling

--- part2/colours/colours/src/main/java/com/ColorApplication/colours/services/impl/ColourPrinterImpl.java
+++ part2/colours/colours/src/main/java/com/ColorApplication/colours/services/impl/ColourPrinterImpl.java
@@ -1,19 +1,25 @@
 package com.ColorApplication.colours.services.impl;
 
+import com.ColorApplication.colours.services.BluePrinter;
 import com.ColorApplication.colours.services.ColourPrinter;
+import com.ColorApplication.colours.services.GreenPrinter;
+import com.ColorApplication.colours.services.RedPrinter;
 
-public class ColourPrinterImpl implements ColourPrinter{
-    private RedPrinter redPrinter;
-    private BluePrinter bluePrinter;
-    private GreenPrinter greenPrinter;
-    public ColourPrinterImpl(){
-        this.redPrinter = new EnglishRedPrinter();
-        this.bluePrinter = new EnglishBluePrinter();
-        this.greenPrinter = new EnglishGreenPrinter();
-    }
+public class ColourPrinterImpl implements ColourPrinter {
+  private RedPrinter redPrinter;
+  private BluePrinter bluePrinter;
+  private GreenPrinter greenPrinter;
 
-    @Overrride
-    public String print(){
-        return String.join(",", redPrinter.print(), bluePrinter.print(), greenPrinter.print());
-    }
+  public
+  ColourPrinterImpl() { // DO NOT DO THIS!!! this is too tight coupaling!! we can't rework with this
+    // since only english language color will appear!
+    this.redPrinter = new EnglishRedPrinter();
+    this.bluePrinter = new EnglishBluePrinter();
+    this.greenPrinter = new EnglishGreenPrinter();
+  }
+
+  @Overrride
+  public String print() {
+    return String.join(",", redPrinter.print(), bluePrinter.print(), greenPrinter.print());
+  }
 }
