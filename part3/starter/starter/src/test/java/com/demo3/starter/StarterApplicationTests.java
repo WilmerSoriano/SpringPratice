22150693fb7d6c5445bc60ba491b5b5d11ed1e54
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-02 01:17:09 -0500

    Will be using the command liine for this

--- part3/starter/starter/src/test/java/com/demo3/starter/StarterApplicationTests.java
+++ part3/starter/starter/src/test/java/com/demo3/starter/StarterApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StarterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
