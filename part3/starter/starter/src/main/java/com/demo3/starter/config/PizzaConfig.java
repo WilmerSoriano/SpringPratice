152a4a4fc514bb762a12f6d604dca6c5e0d88b4d
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-03 00:36:50 -0500

    So now we can add toping without directly changing to code

--- part3/starter/starter/src/main/java/com/demo3/starter/config/PizzaConfig.java
+++ part3/starter/starter/src/main/java/com/demo3/starter/config/PizzaConfig.java
@@ -1,17 +1,17 @@
 package com.demo3.starter.config;
 
+// For all purposes you don't need to understand the annotation properties, we are using lombok
+// library to make data class
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-// For all purposes you don't need to understand the annotation properties, we are using lombok
-// library to make data class
+// This is the key-word our ConfigProperties is going to look for in config/Envi file
 @Configuration
-@ConfigurationProperties(
-    prefix =
-        "pizza") // This is the key our ConfigProperties is going to look for in config/Envi file
+@ConfigurationProperties(prefix = "pizza")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
