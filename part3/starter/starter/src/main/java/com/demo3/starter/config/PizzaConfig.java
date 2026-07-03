a195b08b9aa3227e6af518cf90c3b33481b9750c
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-03 00:30:56 -0500

    Addede config prop library

--- part3/starter/starter/src/main/java/com/demo3/starter/config/PizzaConfig.java
+++ part3/starter/starter/src/main/java/com/demo3/starter/config/PizzaConfig.java
@@ -3,6 +3,7 @@ package com.demo3.starter.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 // For all purposes you don't need to understand the annotation properties, we are using lombok
