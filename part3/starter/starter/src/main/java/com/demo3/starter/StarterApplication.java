b86e1c821f365c90e6858f28ab6dcd2f46fb2ed2
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-03 00:16:27 -0500

    Setting up configuration properties to our pizza

--- part3/starter/starter/src/main/java/com/demo3/starter/StarterApplication.java
+++ part3/starter/starter/src/main/java/com/demo3/starter/StarterApplication.java
@@ -6,9 +6,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Using config properties
 @SpringBootApplication
 @Log
 public class StarterApplication implements CommandLineRunner {
+
+  // 1st. We start here, we inject what we need, in this case in instance of pizzaConfig
+  private PizzaConfig pizzaConfig;
+
+  public StarterApplication(
+      PizzaConfig pizzaConfig) { // But we need a bean to inject this... time to add config
+    this.pizzaConfig = pizzaConfig;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(StarterApplication.class, args);
@@ -16,7 +25,9 @@ public class StarterApplication implements CommandLineRunner {
 
   @Override
   public void run(final String... args) {
-    final PizzaConfig pizzaConfig = new PizzaConfig("tomato", "Cheese", "thin");
+
+    // final PizzaConfig pizzaConfig = new PizzaConfig("tomato", "Cheese", "thin"); REMEMBER, we
+    // don't want to use 'new' keyword we need to inject instead to allow flexibility
     log.info(
         String.format(
             "I want a %s, crust pizza, with %s and %s sauce",
