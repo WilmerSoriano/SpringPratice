3b2e2ee7dd70eb1272c91fe081ec14be4c60c71a
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-09 23:10:56 -0500

    Notes to better understand whats going on.

--- part5/database/src/main/java/com/demo5/database/DatabaseApplication.java
+++ part5/database/src/main/java/com/demo5/database/DatabaseApplication.java
@@ -8,14 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import lombok.extern.java.Log;
+/*
+	This code is only used to test if the connection is for Spring to Mysql is Good.
+	DOES NOT HANDLE:
+	1. creating db
+	2. creating schema
+	3. inserting data
 
+	Rememeber, Spring alone (without code) can already handle that, we just provide
+	the proper information.
+*/
 @SpringBootApplication
 @Log
 public class DatabaseApplication implements CommandLineRunner{
 
-	private final DataSource dataSource; //Injecting a DATA source object as seen by constructor
+	private final DataSource dataSource; //A Spring's object that manages connections to your database. Uses The conifguration in application.properties
 
-	public DatabaseApplication(final DataSource dataSource){
+	public DatabaseApplication(final DataSource dataSource){ //Injects the info
 		this.dataSource = dataSource;
 	}
 
@@ -25,9 +34,9 @@ public class DatabaseApplication implements CommandLineRunner{
 
 	@Override
 	public void run(final String... args){
-		log.info("Datasource: " + dataSource.toString());
+		log.info("IT WORKED LOOK LOOK, Datasource: " + dataSource.toString());//This does not actually test the database. It only proves Spring created the DataSource object.
 		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource); // This is how JDBC allows us to query stuff
-		restTemplate.execute("select 1"); // We are query
+		restTemplate.execute("select 1"); // We are querying to test connection to DB. Expect respond: 1
 	}
 
 }
