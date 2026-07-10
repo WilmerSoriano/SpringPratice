ed381872d2667467495415809412c89194620f16
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-09 21:24:26 -0500

    Added everything from part4 project

--- part5/database/src/main/java/com/demo5/database/DatabaseApplication.java
+++ part5/database/src/main/java/com/demo5/database/DatabaseApplication.java
@@ -1,13 +1,33 @@
 package com.demo5.database;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.extern.java.Log;
 
 @SpringBootApplication
-public class DatabaseApplication {
+@Log
+public class DatabaseApplication implements CommandLineRunner{
+
+	private final DataSource dataSource; //Injecting a DATA source object as seen by constructor
+
+	public DatabaseApplication(final DataSource dataSource){
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args){
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource); // This is how JDBC allows us to query stuff
+		restTemplate.execute("select 1"); // We are query
 	}
 
 }
