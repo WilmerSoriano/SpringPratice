d82ab054f9716b0c9265524a5906ac180c72cfa6
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-09 21:05:03 -0500

    Fixed typo

--- part4/database/src/main/java/com/demo4/database/DatabaseApplication.java
+++ part4/database/src/main/java/com/demo4/database/DatabaseApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import lombok.extern.java.Log;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @Log
 public class DatabaseApplication implements CommandLineRunner{
 
-	private final DataSource DataSource;
+	private final DataSource dataSource; //Injecting a DATA source object as seen by constructor
 
 	public DatabaseApplication(final DataSource dataSource){
 		this.dataSource = dataSource;
@@ -24,8 +25,8 @@ public class DatabaseApplication implements CommandLineRunner{
 	@Override
 	public void run(final String... args){
 		log.info("Datasource: " + dataSource.toString());
-		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
-		restTemplate.execute("select 1");
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource); // This is how JDBC allows us to query stuff
+		restTemplate.execute("select 1"); // We are query
 	}
 
 }
