ac9a6e2d8b10f79346cd64fdb6d5af3983f13ae0
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-16 14:30:14 -0500

    Added the jdbc templet injection

--- part6/database/src/main/java/com/demo6/database/dao/impl/AuthorDaoImpl.java
+++ part6/database/src/main/java/com/demo6/database/dao/impl/AuthorDaoImpl.java
@@ -1,7 +1,13 @@
 package com.demo6.database.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.demo6.database.dao.AuthorDao;
 
 public class AuthorDaoImpl implements AuthorDao{
-    
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthorDaoImpl(final JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
--- part6/database/src/main/java/com/demo6/database/dao/impl/BookDoaImpl.java
+++ part6/database/src/main/java/com/demo6/database/dao/impl/BookDoaImpl.java
@@ -1,7 +1,13 @@
 package com.demo6.database.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.demo6.database.dao.BookDao;
 
 public class BookDoaImpl implements BookDao{
-    
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(final JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
