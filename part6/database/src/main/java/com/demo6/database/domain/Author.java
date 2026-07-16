afd78e3f2b0edc412709ec7c16937dae2a916a39
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-07-16 14:00:08 -0500

    These are first siimply the service layer. This is what our Spring works on.

--- part6/database/src/main/java/com/demo6/database/domain/Author.java
+++ part6/database/src/main/java/com/demo6/database/domain/Author.java
@@ -1,5 +1,19 @@
 package com.demo6.database.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Author {
     
+    private Long id;
+
+    private String name;
+
+    private Integer age;
 }
