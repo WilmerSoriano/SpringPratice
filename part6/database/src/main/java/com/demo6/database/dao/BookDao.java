6549ba4a3322d5f0da87dca042181d46fa0a4e17
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-08-04 22:52:55 -0500

    Fixed issue in naming constructor and added the create method

--- part6/database/src/main/java/com/demo6/database/dao/BookDao.java
+++ part6/database/src/main/java/com/demo6/database/dao/BookDao.java
@@ -1,5 +1,5 @@
 package com.demo6.database.dao;
-
+import com.demo6.database.domain.Book;
 public interface BookDao {
-    
+    void create(Book book);
 }
--- part6/database/src/main/java/com/demo6/database/dao/impl/BookDoaImpl.java
+++ part6/database/src/main/java/com/demo6/database/dao/impl/BookDoaImpl.java
@@ -3,11 +3,22 @@ package com.demo6.database.dao.impl;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.demo6.database.dao.BookDao;
+import com.demo6.database.domain.Book;
 
 public class BookDoaImpl implements BookDao{
     private final JdbcTemplate jdbcTemplate;
 
-    public BookDaoImpl(final JdbcTemplate jdbcTemplate){
+    public BookDoaImpl(final JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(Book book){
+        jdbcTemplate.update(
+            "INSERT INTO books (isbn, title, author_id) VALUES (?, ?, ?)",
+            book.getIsbn(),
+            book.getTitle(),
+            book.getAuthorId()
+        );
     }
 }
