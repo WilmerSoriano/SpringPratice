2ecd08945d58aa6ad8074c5d0a2efd08692c3825
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-08-06 20:42:40 -0500

    1st creating read for author

--- part6/database/src/main/java/com/demo6/database/dao/AuthorDao.java
+++ part6/database/src/main/java/com/demo6/database/dao/AuthorDao.java
@@ -1,7 +1,11 @@
 package com.demo6.database.dao;
 
+import java.util.Optional;
+
 import com.demo6.database.domain.Author;
 // Remember this interface does not change, and it can be used by many other other database service other then MySQL
 public interface AuthorDao {
-    void create(Author author);
+    void create(Author author); // This method create an Author
+
+    Optional<Author> findOne(long l); // This method looks for an Author with long ID
 }
---
+++ part6/database/src/test/java/com/demo6/database/dao/impl/AuthorDaoImplTest.java
@@ -0,0 +1,41 @@
+package com.demo6.database.dao.impl;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.demo6.database.dao.impl.AuthorDaoImpl;
+import com.demo6.database.domain.Author;
+// This is testing if my AuthorDoaImpl.java file actually works! But instead of MySQl we are using a Mock version
+@ExtendWith(MockitoExtension.class)
+public class AuthorDaoImplTest {
+    
+    @Mock//Mock - Pretend this dependency exists, but don't actually perform the real action.
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private AuthorDaoImpl underTest;
+
+    @Test
+    public void testThatCreateAuthorGeneratesCorrectSql(){
+        
+        Author author = Author.builder().id(1L).name("Abigail Rosde").age(80).build();
+
+        underTest.create(author);
+        verify(jdbcTemplate).update(
+            eq("INSERT INTO authors (id, name, age) VALUES (?,?,?)"),
+            eq(1L), eq("Abigail Rosde"), eq(80)
+        );
+    }
+
+    @Test
+    public void testThatFindOneGeneratesTheCorrectSql(){
+        underTest.findOne(1L);
+    }
+}
