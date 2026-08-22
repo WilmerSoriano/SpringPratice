8df2af0e7d0a18a09785fe9eda12bb50690ba573
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-08-21 21:06:50 -0500

    ignore this test for now

--- part7/database/src/test/java/com/demo6/database/TestDataUtil.java
+++ part7/database/src/test/java/com/demo6/database/TestDataUtil.java
@@ -1,34 +1,34 @@
-package com.demo6.database;
+// package com.demo6.database;
 
-import com.demo6.database.domain.Author;
-import com.demo6.database.domain.Book;
-// Test Data Utility allowes me to create small samples of obj to be tested in dao/impl files.
-public final class TestDataUtil {
-    private TestDataUtil(){
+// import com.demo6.database.domain.Author;
+// import com.demo6.database.domain.Book;
+// // Test Data Utility allowes me to create small samples of obj to be tested in dao/impl files.
+// public final class TestDataUtil {
+//     private TestDataUtil(){
 
-    }
+//     }
     
-    public static Author createTestAuthor() {
-        return Author.builder().id(1L).name("Abigail Rosde").age(80).build();
-    }
+//     public static Author createTestAuthor() {
+//         return Author.builder().id(1L).name("Abigail Rosde").age(80).build();
+//     }
 
-    public static Author createTestAuthorB() {
-        return Author.builder().id(2L).name("Matt Path").age(22).build();
-    }
+//     public static Author createTestAuthorB() {
+//         return Author.builder().id(2L).name("Matt Path").age(22).build();
+//     }
 
-    public static Author createTestAuthorC() {
-        return Author.builder().id(3L).name("Hanse Solo").age(32).build();
-    }
+//     public static Author createTestAuthorC() {
+//         return Author.builder().id(3L).name("Hanse Solo").age(32).build();
+//     }
     
-    public static Book createTestBook() {
-        return Book.builder().isbn("bn23j4k1").title("Me sad").authorId(1L).build();
-    }
+//     public static Book createTestBook() {
+//         return Book.builder().isbn("bn23j4k1").title("Me sad").authorId(1L).build();
+//     }
 
-    public static Book createTestBookB() {
-        return Book.builder().isbn("bn23j4k2").title("Why Again").authorId(1L).build();
-    }
+//     public static Book createTestBookB() {
+//         return Book.builder().isbn("bn23j4k2").title("Why Again").authorId(1L).build();
+//     }
 
-    public static Book createTestBookC() {
-        return Book.builder().isbn("bn23j4k3").title("greg").authorId(1L).build();
-    }
-}
+//     public static Book createTestBookC() {
+//         return Book.builder().isbn("bn23j4k3").title("greg").authorId(1L).build();
+//     }
+// }
