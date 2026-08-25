82f84a200f5c51871088d9447ca22785d8b92fc1
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-08-25 17:18:36 -0500

    Added a comment to repo author to remember where these functions/methods are coming from

--- part7/database/src/main/java/com/demo6/database/repositories/AuthorRepository.java
+++ part7/database/src/main/java/com/demo6/database/repositories/AuthorRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.demo6.database.domain.Author;
 
 @Repository // The same as Annotation Component, but is descripbed as a repostiory bean
-public interface AuthorRepository extends CrudRepository<Author, Long>{
+public interface AuthorRepository extends CrudRepository<Author, Long>{ // The CrudRepository alreadyu gave us method to work with!
     
 }
