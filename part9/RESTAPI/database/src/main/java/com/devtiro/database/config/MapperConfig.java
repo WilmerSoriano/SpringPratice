9355673de9e07970b0562c0c9f42f3b0ab6de9ce
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-09-07 20:53:56 -0500

    needed to allow nested object with book(author)

--- part9/RESTAPI/database/src/main/java/com/devtiro/database/config/MapperConfig.java
+++ part9/RESTAPI/database/src/main/java/com/devtiro/database/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.devtiro.database.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,22 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);// This well help us match nested object
+        return modelMapper();
     }
 }
+// Example of Nested object in JSON:
+
+//  HTTP PUT localhost:8080/books/123-456-9
+//  
+//  {
+//      "isbn": "123-456-9",
+//      "title": "Shadow",
+//      "author"{                          <=this is what I mean nested object, so author is created using the CREATE book
+//          "name" : "Mike Jones"
+//          "age" : 18
+//      }
+//  }
+
+// The ID will be generated
