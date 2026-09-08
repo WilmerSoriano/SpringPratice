dfda8b1ba543277e457d745afb03f356ed09044b
WilmerSoriano <official_Wilmer.2020@outlook.com>
2026-09-07 19:54:17 -0500

    Removed Repostiory annotation. Not needed

--- part9/RESTAPI/database/src/main/java/com/devtiro/database/repositories/BookRepository.java
+++ part9/RESTAPI/database/src/main/java/com/devtiro/database/repositories/BookRepository.java
@@ -2,8 +2,6 @@ package com.devtiro.database.repositories;
 
 import com.devtiro.database.domain.entities.BookEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BookRepository extends CrudRepository<BookEntity, String> {
 }
